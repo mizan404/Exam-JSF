ac80bc5b992297e00c997cc431432382040b6d63
Muhammad Mizanur Rahman <mizanthecoder@gmail.com>
2020-10-20 18:59:23 +0600

    20.10.2020

---
+++ classtest/src/java/com/mizanthecoder/StudentController.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mizanthecoder;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Students
+ */
+@ManagedBean
+@RequestScoped
+public class StudentController {
+
+    private Student student;
+    private String result;
+    private String[] education;
+    StudentService service = new StudentService();
+    public List<Student> students;
+
+    public void saveStudent() {
+        service.save(student);
+        result = "Insert Success";
+    }
+
+    public void displayStudent(int id) {
+        student = service.getById(id);
+
+    }
+
+    public void updateStudent() {
+        service.update(student);
+        result = "Update Success";
+    }
+
+    public void delete(int id) {
+        service.del(new Student(id));
+        result = "Delete Success";
+
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String[] getEducation() {
+        return education;
+    }
+
+    public void setEducation(String[] education) {
+        this.education = education;
+    }
+
+    public StudentService getService() {
+        return service;
+    }
+
+    public void setService(StudentService service) {
+        this.service = service;
+    }
+
+    public List<Student> getStudents() {
+        students = service.getAll();
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+}
---
+++ classtest/src/java/com/mizanthecoder/StudentService.java
@@ -0,0 +1,56 @@
+
+package com.mizanthecoder;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+
+public class StudentService {
+
+    public void save(Student student) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction tr = session.beginTransaction();
+        session.save(student);
+        tr.commit();
+
+    }
+
+    public void update(Student student) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction tr = session.beginTransaction();
+        session.saveOrUpdate(student);
+        tr.commit();
+
+    }
+
+    public void del(Student student) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction tr = session.beginTransaction();
+        session.delete(student);
+        tr.commit();
+
+    }
+
+    public Student getById(int id) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction tr = session.beginTransaction();
+        Student student = (Student) session.get(Student.class, id);
+        tr.commit();
+        return student;
+    }
+
+    public List<Student> getAll() {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction tr = session.beginTransaction();
+        List<Student> list = session.createQuery("From Student").list();
+        tr.commit();
+        return list;
+    }
+}
